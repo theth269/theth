187cb9db060c2bdbf490cc33a78f97c1c19db47f
The08 <the08012008@gmail.com>
2025-10-20 21:59:28 +0700

    Khởi tạo dự án Galaxy

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ const galaxyParameters = {
   outsideColor: new THREE.Color(0x48b8b8),
 };
 
-const defaultHeartImages = Array.from({ length: 58 }, (_, i) => `images/${i + 1}.jpg`);
+const defaultHeartImages = Array.from({ length: 83 }, (_, i) => `images/${i + 1}.jpg`);
 
 const heartImages = [
   ...(window.dataCCD?.data?.heartImages || []),
